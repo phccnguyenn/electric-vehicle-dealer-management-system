732ecd4df45df1450a3b11bf838186cbc431047c
phccnguyenn <phccnguyenn2554@gmail.com>
2025-09-19 11:42:22 +0700

    Upload codebase

---
+++ src/main/java/com/evdealer/ev_dealer_management/EvDealerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.evdealer.ev_dealer_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvDealerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvDealerManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evdealer/ev_dealer_management/EvDealerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evdealer.ev_dealer_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvDealerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
