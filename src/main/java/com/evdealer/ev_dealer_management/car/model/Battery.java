07ceb5408ad604c7cdcab6d1deca40fe96586d4c
Đạt Trần Quốc <datkimanh23419@outlook.com>
2025-09-24 01:25:32 +0700

    entity of CarEntity

---
+++ src/main/java/com/evdealer/ev_dealer_management/car/model/Battery.java
@@ -0,0 +1,32 @@
+package com.evdealer.ev_dealer_management.car.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "batteries")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Battery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "battery_id", nullable = false, unique = true)
+    private String batteryId ;
+    @Column(name = "battery_type",length = 100, nullable = false)
+    private String batteryType ;
+    @Column(name = "chargin_duration", nullable = false)
+    private String chargingDuration ;
+    @Column(name = "duration_use", nullable = false)
+    private String durationUse ;
+    @Column(name = "weight", nullable = false,precision = 7, scale = 2)
+    private String Weight ;
+
+    @OneToMany(mappedBy = "battery",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<CarConfig> carConfigs =new ArrayList<>() ;
+}
