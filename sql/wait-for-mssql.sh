4c35f46be2fb92e6a808a4f10a3448fbe5570449
phccnguyenn <phccnguyenn2554@gmail.com>
2025-09-24 10:15:05 +0700

    Update .sh file

--- sql/wait-for-mssql.sh
+++ sql/wait-for-mssql.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 SERVER="evdealer-mssql,1433"
+USER="sa"
 MSSQL_SA_PASSWORD='6$3S6zPKpJ+Y2-*J'
 
 # Wait until SQL Server is ready
 echo "Waiting for SQL Server to start..."
-sleep 15s
+sleep 10s
 
 # Run all SQL scripts in folder
 for file in /opt/sql/*.sql; do
     echo "Running $file..."
-    /opt/mssql-tools/bin/sqlcmd -S $SERVER -U sa -P "$MSSQL_SA_PASSWORD" -i "$file"
+    /opt/mssql-tools/bin/sqlcmd -S $SERVER -U $USER -P "$MSSQL_SA_PASSWORD" -i "$file"
 done
 
-echo "SQL scripts run successfully"
+echo "SQL Database initialization completed."
