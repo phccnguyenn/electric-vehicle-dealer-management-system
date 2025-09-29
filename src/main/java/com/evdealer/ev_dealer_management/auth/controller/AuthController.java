d8303f5e99d026bb764d3ec4237dab85746b788b
phccnguyenn <phccnguyenn2554@gmail.com>
2025-09-29 10:37:00 +0700

    [UPLOAD] register logic

--- src/main/java/com/evdealer/ev_dealer_management/auth/controller/AuthController.java
+++ src/main/java/com/evdealer/ev_dealer_management/auth/controller/AuthController.java
@@ -1,20 +1,27 @@
 package com.evdealer.ev_dealer_management.auth.controller;
 
+import com.evdealer.ev_dealer_management.auth.model.User;
 import com.evdealer.ev_dealer_management.auth.model.dto.AuthRequest;
 import com.evdealer.ev_dealer_management.auth.model.dto.AuthResponse;
+import com.evdealer.ev_dealer_management.auth.model.dto.RegisterRequest;
+import com.evdealer.ev_dealer_management.auth.model.dto.RegisterResponse;
 import com.evdealer.ev_dealer_management.auth.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
 
     @PostMapping("/authenticate")
@@ -24,17 +31,17 @@ public class AuthController {
         return ResponseEntity.ok(authService.authenticate(authRequest));
     }
 
-    /* @PostMapping("/admin/register")
-    public ResponseEntity<AuthResponse> register(
-            @ResponseBody AuthRequest authRequest
-    ) {
-        return ResponseEntity.ok(authService.registerUser(authRequest));
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest user) {
+        RegisterResponse response = authService.register(user);
+        return ResponseEntity.ok(response);
     }
-    */
+
     @PostMapping("/refresh-token")
     public void refreshToken(
             HttpServletRequest request,
             HttpServletResponse response) throws IOException {
         authService.refreshToken(request, response);
     }
+
 }
