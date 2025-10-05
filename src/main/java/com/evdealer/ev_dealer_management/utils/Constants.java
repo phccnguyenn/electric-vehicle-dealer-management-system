ef5441ff5f7c8ff848e0f48cb5d9cc7aab39041c
phccnguyenn <phccnguyenn2554@gmail.com>
2025-10-05 11:30:37 +0700

    .

--- src/main/java/com/evdealer/ev_dealer_management/utils/Constants.java
+++ src/main/java/com/evdealer/ev_dealer_management/utils/Constants.java
@@ -4,5 +4,8 @@ public final class Constants {
     public static final class ErrorCode {
         public final static String USERNAME_NOT_FOUND = "USERNAME_NOT_FOUND";
         public final static String USERNAME_ALREADY_EXIST = "USERNAME_ALREADY_EXIST";
+
+        public final static String CATEGORY_NOT_FOUND = "CATEGORY_NOT_FOUND";
+        public final static String COLOR_NOT_FOUND = "CATEGORY_NOT_FOUND";
     }
 }
