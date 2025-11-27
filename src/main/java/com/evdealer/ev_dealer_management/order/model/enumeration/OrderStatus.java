a8bc01957d9d7af2325c1a48deb1e4e2f8084ce5
phccnguyenn <phccnguyenn2554@gmail.com>
2025-11-27 23:34:27 +0700

    [refactory] fix contraint price of car model followed price program

--- src/main/java/com/evdealer/ev_dealer_management/order/model/enumeration/OrderStatus.java
+++ src/main/java/com/evdealer/ev_dealer_management/order/model/enumeration/OrderStatus.java
@@ -2,7 +2,7 @@ package com.evdealer.ev_dealer_management.order.model.enumeration;
 
 public enum OrderStatus {
     PENDING,
-    DEMO_AVAILABLE,
+    //DEMO_AVAILABLE,
     APPROVED,
     REJECTED,
     IN_DELIVERY,
